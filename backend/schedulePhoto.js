205d3f3a245a76be7cf72a9cef37b019e9ae22c0
DaHuntr9 <hunterdstudent@gmail.com>
2020-05-10 05:22:58 -0400

    video function added

--- backend/schedulePhoto.js
+++ backend/schedulePhoto.js
@@ -1,28 +1,29 @@
 /**
- * This section of code is directed exclusivly for the 30 Min timers. 
+ * This section of code is directed exclusively for the 30 Min timers. 
  */
 const PiCamera = require('pi-camera');
 const schedule = require('node-schedule');
 
-const photoName
+const fileName
 const myCamera
 
-// Create a Timer observer that checks to ensure that the time is in 30 min invervals. 
+// Create a Timer observer that checks to ensure that the time is in 30 min intervals. 
 var rule = new schedule.RecurrenceRule();
 rule.minute = [0,30,59];
 var scheduleJob = schedule.scheduleJob(rule, function(){
     console.log('Automatic Photo taken at' + Date.now());
-    // Call functions to take photo and perhaps post somthing to a discord bot later. 
-    takePhotoScheduled(photoName,myCamera);
+    // Call functions to take photo and perhaps post something to a discord bot later. 
+    takePhotoScheduled(fileName,myCamera);
+    //takeVideoScheduled(fileName,myCamera);
 });
 
 // takePhotoScheduled function to take photos based on a 30 min interval. 
-function takePhotoScheduled(photoName,myCamera){
+function takePhotoScheduled(fileName,myCamera){
     // reCreate objects to perform photo taking and file saving.
-    photoName = Date.now();
+    fileName = Date.now();
     myCamera = new PiCamera({
         mode: 'photo',
-        output: `${ __dirname }/${photoName}.jpg`,
+        output: `${ __dirname }/${fileName}.jpg`,
         width: 1920,
         height: 1080,
         nopreview: true,
@@ -31,10 +32,52 @@ function takePhotoScheduled(photoName,myCamera){
     // Photo Taking command. 
     myCamera.snap().then((result) => {
         console.log("The server took a photo!")
-        console.log('Photo taken as:'+photoName+'.jpg')
+        console.log('Photo taken as:'+fileName+'.jpg')
     })
     .catch((error) => {
         console.log(error)
         console.log('Photo was not taken please check server console.')
     });
 }
+
+
+/**
+ * Below are the functions to create a time lapse. 
+ */
+
+function takeVideoScheduled(fileName,myCamera){
+    // reCreate objects to perform photo taking and file saving.
+    fileName = Date.now();
+    const myCamera = new PiCamera({
+        mode: 'video',
+        output: `${ __dirname }/${fileName}.h264`,
+        width: 1920,
+        height: 1080,
+        timeout: 5000, // Record for 5 seconds
+        nopreview: true,
+      });
+    
+    // Photo Taking command. 
+    myCamera.record().then((result) => {
+        console.log("The server took a photo!")
+        console.log('Photo taken as:'+fileName+'.jpg')
+    })
+    .catch((error) => {
+        console.log(error)
+        console.log('Photo was not taken please check server console.')
+    });
+}
+
+/**
+ * The following function is to conduct the rotation of the platform in which the camera is operating on.
+ * This will act simultaneously with the video recording so that a moving time lapse can be built of the plant.
+ * 
+ * Requires: 
+ * onoff which is only able to be installed on linux systems. 
+ *              npm install onoff 
+ */
+
+// TODO create function to handle RP IO pins with servo. 
+//  function rotateCamera(){
+
+//  }
